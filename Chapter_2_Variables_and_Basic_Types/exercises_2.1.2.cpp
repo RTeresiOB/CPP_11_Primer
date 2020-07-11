3b867d260517b038287f95d6506bd7f173158b25
RTeresiOB <robert.teresi@yale.edu>
2020-07-11 11:44:16 -0600

    A Few More Exercises

--- Chapter_2_Variables_and_Basic_Types/exercises_2.1.2.cpp
+++ Chapter_2_Variables_and_Basic_Types/exercises_2.1.2.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 
 using namespace std;
+
 //////////////////////////////////////////////////////////
 // Working with signed and unsigned ints.
 //
 // What will be the results of the following expressions?
 //
 // Take a "guess" then run program to find out if you were correct
+//////////////////////////////////////////////////////////
+
 int main(){
     unsigned int u = 10, u2 = 42;
     cout << u2 - u << endl;
--- Chapter_2_Variables_and_Basic_Types/exercises_2.1.3.cpp
+++ Chapter_2_Variables_and_Basic_Types/exercises_2.1.3.cpp
@@ -7,7 +7,8 @@ int main(){
     // Writing messages with escape sequences.
     cout << "Who goes with F\145rgus?\012" << endl;
 
-    // Using escape sequences, write 2M, then write 2 and M on separate lines, then 2 tab M newline
+    // Using escape sequences, write 2M, then write 2 and M on separate lines
+    // then 2 tab M newline
     cout << "\62\115" << endl;
     cout << "\62\n\115" << endl;
     cout << "\62\t\115\n" << endl;
